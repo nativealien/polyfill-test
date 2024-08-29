5e6b1e89dc26ecb76744f638dad937e9e5b0b528
nativealien <oskar@assarsson.nu>
2024-08-29 14:15:06 +0200

    added connect tezos

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
+import { TezosToolkit } from '@taquito/taquito'
+import { BeaconWallet } from '@taquito/beacon-wallet'
+import { NetworkType } from "@airgap/beacon-dapp";
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+  const [tezKey, setTezKey] = useState<any>(null)
+
+  const Tezos = new TezosToolkit("https://ghostnet.smartpy.io")
+  const loadWallet = async (check: boolean) => {
+        const newWallet = new BeaconWallet({ name: "PolyTest",
+                                             network: {
+                                                 type: NetworkType.GHOSTNET }})
+        await newWallet.requestPermissions().then( async () => {
+            Tezos.setWalletProvider(newWallet)
+            const pkh = await Tezos.wallet.pkh(); 
+            setTezKey(pkh)
+          })
+  }
 
   return <div className="app">
     <h1>Poly Fills</h1>
+
+    <button style={{
+      height: "50px",
+      width: "150px"}}
+      onClick={ () =>
+        loadWallet(true)
+      }>Connect Tezos</button>
+      {tezKey && <p>{tezKey}</p>}
+
+
   </div>
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  define: {
+    global: {},
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  plugins: [nodePolyfills(), react()],
+  resolve: {
+    alias: {
+      stream: "stream-browserify",
+      os: "os-browserify/browser",
+      util: "util",
+      process: "process/browser",
+      buffer: "buffer",
+    },
+  },
+});
